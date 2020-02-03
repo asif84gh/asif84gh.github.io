a90a251ed89ce48228c68784e5d0388dedadd011
asif84gh <50854933+asif84gh@users.noreply.github.com>
2020-02-03 12:40:47 +0530

    Add files via upload
    Finalled

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
 
-echo "Hello World!";
+echo  "<h1>Hello World!!</h1>";
+print "Hello World!!";
+
+$name = "Shahyan";
+
+echo "Hello<h1>".$name."</h1>";
+
+$a = 5;
+$b = 10.5;
+$c = $a + $b;
+echo $c;
+
+if($a%2==0)
+{
+	echo "Number is even.<br>";
+}
+else
+{
+	echo "Number is Odd.<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
